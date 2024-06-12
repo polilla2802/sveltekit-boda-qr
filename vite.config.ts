c868aeab1591bd0d8cfad0c1d43d9ede45f1e5d9
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-12 02:05:04 -0500

    Removed eslint + env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+// Get the port from the environment variables, default to 8000 if not set
+const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+      port: port,
+    },
+    preview: {
+      port: port,
+    },
 });
