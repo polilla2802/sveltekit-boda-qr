561280a0df6006d0931d2eb6591a34680abbdc43
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-12 12:12:35 -0500

    Added Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  console.log($page.url.pathname);
+</script>
+
+<nav>
+  <div class="left">
+    <a class:active={$page.url.pathname === "/"} href="/">Mi Boda</a>
+  </div>
+  <div class="right">
+    <a href="/signup">Inicia Sesión</a>
+    <a href="/create">Registrate</a>
+  </div>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    padding: 2rem;
+    align-items: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+    display: inline-block;
+  }
+
+  a + a {
+    margin-left: 1rem;
+  }
+
+  .active {
+    font-weight: bold;
+  }
+
+  .right {
+    margin-left: auto;
+  }
+
+  .right a {
+    border: 1px solid black;
+    padding: 0.5rem 1rem;
+    border-radius: 3px;
+  }
+</style>
