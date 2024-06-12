e157845faf0fa8c62c108780212de55cc684f62b
Jose Manuel Alarcon <78869984+polilla2802@users.noreply.github.com>
2024-06-12 17:24:56 -0500

    Added QR code

--- src/routes/user/[id]/+page.svelte
+++ src/routes/user/[id]/+page.svelte
@@ -5,9 +5,26 @@
 	// Use the `$page` store to get the data returned by the load function
 	const { user } = $page.data.user;
 
+	const userId = user.id;
+
+	let qrCode: string;
+
+	async function getQRCode() {
+		try {
+			const response = await fetch(
+				`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://sveltekit-boda-qr.vercel.app/api/user/${userId}` +
+					user.id
+			);
+			qrCode = response.url;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	// Optional: If you need to perform any action on mount
 	onMount(() => {
 		console.log('Component has mounted', user);
+		getQRCode();
 	});
 </script>
 
@@ -21,6 +38,10 @@
 		<p><strong>Teléfono:</strong> {user.phoneNumber}</p>
 		<p><strong>Genero:</strong> {user.gender}</p>
 		<p><strong>Edad:</strong> {user.age}</p>
+	{/if}
+	<br />
+	{#if qrCode}
+		<img src={qrCode} width="150" height="150" alt="QR code" />
 	{/if}
 </main>
 
